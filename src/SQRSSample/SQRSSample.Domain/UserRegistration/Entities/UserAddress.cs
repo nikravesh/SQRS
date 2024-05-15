fab124c52e61317f5fadcf8b1bcf1664639216ee
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-05-15 23:51:09 +0330

    Added UserAddress Entity

---
+++ src/SQRSSample/SQRSSample.Domain/UserRegistration/Entities/UserAddress.cs
@@ -0,0 +1,31 @@
+﻿using SQRSSample.Domain.Entity;
+
+namespace SQRSSample.Domain.UserRegistration.Entities;
+
+public class UserAddress : Entity<int>
+{
+    public uint ZipCode { get; private set; }
+    public string Province { get; private set; }
+    public string City { get; private set; }
+    public string BuildingNo { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string CellPhone { get; private set; }
+
+    private UserAddress() { }
+
+    public UserAddress(
+        uint zipCode,
+        string prvince,
+        string city,
+        string buildingNo,
+        string phoneNumber,
+        string cellPhone)
+    {
+        ZipCode = zipCode;
+        Province = prvince;
+        City = city;
+        BuildingNo = buildingNo; 
+        PhoneNumber = phoneNumber;
+        CellPhone = cellPhone;
+    }
+}
--- src/SQRSSample/SQRSSample.Domain/UserRegistration/Entities/UserRegistration.cs
+++ src/SQRSSample/SQRSSample.Domain/UserRegistration/Entities/UserRegistration.cs
@@ -1,4 +1,25 @@
-﻿namespace SQRSSample.Domain.UserRegistration.Entities;
-public class UserRegistration
+﻿using System.Collections.Immutable;
+
+using SQRSSample.Domain.Entity;
+
+namespace SQRSSample.Domain.UserRegistration.Entities;
+public class UserRegistration : AggregateRoot<int>
 {
+    public string FirstName { get; private set; }    
+
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public IReadOnlyList<UserAddress> UserAddresses => _userAddresses.ToImmutableList();
+    private List<UserAddress> _userAddresses => new();
+
+    private UserRegistration() { }
+
+    private UserRegistration(string firstName, string lastName, string email, string password)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
 }
